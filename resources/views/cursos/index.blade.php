a7b7737857080d383b426ac4f1659776dcf97f13
vanessa <gra0298@gmail.com>
2022-04-28 21:06:01 -0500

    se agregaron nuevos cursos

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -9,7 +9,7 @@
         <div class="col-sm">
             <br>
             <div class="card text-center" style="width: 18rem; margin-top:20px">
-                <img style="height:50px; width:250px; margin:15px" src="{{ Storage::url($alias->imagen) }}" class="card-img-top mx-auto d-block" alt="Imagen del curso">
+                <img style="height:180px; width:180px; margin:15px" src="{{ Storage::url($alias->imagen) }}" class="card-img-top mx-auto d-block" alt="Imagen del curso">
                 <div class="card-body">
                     <h5 class="card-title">{{$alias->nombre}}</h5>
                     <p class="card-text">{{$alias->descripcion}}</p>
